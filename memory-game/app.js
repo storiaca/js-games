a32de1791ac887bceae9a7d692a8b73fcfda882a
storiaca <storiaca@gmail.com>
2020-04-27 16:13:40 +0200

    images

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,3 +1,48 @@
-document.addEventListener("DOMContebtLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   // card options
+  const cardArray = [
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "ice-ceam",
+      img: "images/ice-ceam.png",
+    },
+    {
+      name: "ice-ceam",
+      img: "images/ice-ceam.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+  ];
+
+  const grid = document.querySelector(".grid");
+  // create your board
 });
