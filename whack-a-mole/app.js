2e9de159a272afea1f17e230970d715109142c4e
Aleksandar Ristic <storiaca@gmail.com>
2020-05-04 11:43:13 +0200

    Whack a mole finished

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -1,20 +1,23 @@
 const square = document.querySelectorAll(".square");
-const mole = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
 const timeLeft = document.querySelector("#time-left");
 let score = document.querySelector("#score");
 
 let result = 0;
+let currentTime = timeLeft.textContent;
 
 function randomSquare() {
-  square.forEach((className) => {
-    className.classList.remove("mole");
-  });
 
-  let randomPosition = square[Math.floor(Math.random() * 9)];
-  randomPosition.classList.add("mole");
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
 
-  // assign the id of the randomPosition to hitPosition
-  hitPosition = randomPosition.id;
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole')
+
+  //assign the id of the randomPosition to hitPosition for us to use later
+  hitPosition = randomPosition.id
+
 }
 
 square.forEach(id => {
@@ -25,3 +28,22 @@ square.forEach(id => {
     }
   })
 })
+
+function moveMole() {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown(){
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if(currentTime === 0) {
+    clearInterval(timerId);
+    alert("GAME OVER! Your final score is" + result)
+  }
+}
+
+let timerId = setInterval(countDown, 1000)
